ad5c741165cf35e27b13317a4720254b42d61d86
Craig Hollabaugh <craig@hollabaugh.com>
2018-08-21 02:30:22 -0600

    reading GPIOA, if S1A1 pressed, blinking pauses

--- src/babysteps/mcp23017/mcp23017.cydsn/main.c
+++ src/babysteps/mcp23017/mcp23017.cydsn/main.c
@@ -2,13 +2,20 @@
 
 /* uint8_t ms included below so debugging with locals/watch is possible */
 /* printf doesn't go anywhere, or does it? */
+/* using bank=0 */
 
 #include <stdio.h>
 #include "project.h"
 
 #define IOADDR 0x20
+
+#define IODIRA 0x00
+#define IOPOLA 0x02
+
 #define IODIRB 0x01
+#define GPIOA  0x12
 #define GPIOB  0x13
+#define GPPUA  0x0C
 
 void regWrite(uint8_t addr, uint8_t reg, uint8_t value) {
     uint8_t wBuf[2] = {0};
@@ -30,16 +37,23 @@ uint8_t regRead(uint8_t addr, uint8_t reg) {
 }
 
 int main(void)
-{
+{   
     CyGlobalIntEnable;
     
     MCP23017_Start();
-    
-    regWrite(IOADDR,IODIRB,0xFE); // bit 0 to output
+
+    regWrite(IOADDR,IODIRA,0xFF);   // set port A to input
+    regWrite(IOADDR,IOPOLA,0xFF);   // invert logic on port A, 1 means switch pressed
+    regWrite(IOADDR,GPPUA,0xFF);    // add pull-up to all port A
+    regWrite(IOADDR,IODIRB,0xFE);   // bit 0 to output
+    regWrite(IOADDR,GPIOB,0x01);    // led on
+
     for(;;) {
         regWrite(IOADDR,GPIOB,0x01); // led on
         CyDelay(500);
         regWrite(IOADDR,GPIOB,0x00); // led off
         CyDelay(500);
+        
+        while (regRead(IOADDR,GPIOA) & 0x01) {};
     }
 }
