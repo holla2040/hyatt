e1dba2b3c8bc1d852a8dda3049df06d536f45d3b
Craig Hollabaugh <craig@hollabaugh.com>
2018-09-05 03:02:54 -0600

    changed 96 to 80 point laid out buttons added override, status and feed fields status has 4 background colors, green, blue, yellow, red for grbl state line field now editable, added enter button to send line to grbl added display download direct hardware passthru circuit using 059 switch

--- src/babysteps/nextion/nextion.cydsn/main.c
+++ src/babysteps/nextion/nextion.cydsn/main.c
@@ -1,8 +1,10 @@
 /* this project uses nextion display loaded with droSimple
 
-   add m library gcc command line -lm
+    add m library gcc command line -lm
     change heap size to 0x200 in cydwr System
     add linker command line -u_printf_float
+    
+    https://nextion.itead.cc/resources/documents/instruction-set/
 
 */
 #include "project.h"
@@ -24,19 +26,11 @@ CY_ISR(updateHandler) {
 
 void displayUpdate() {
     char line[100];
-    sprintf(line,"x.txt=\"%9.4f\"\xff\xff\xffy.txt=\"%9.4f\"\xff\xff\xffz.txt=\"%9.4f\"\xff\xff\xff",x,y,z);
+    sprintf(line,"ref_stop\xff\xff\xffx.txt=\"%9.4f\"\xff\xff\xffy.txt=\"%9.4f\"\xff\xff\xffz.txt=\"%9.4f\"\xff\xff\xffref_star\xff\xff\xff",x,y,z);
     display_PutString(line);
     displayPending = false;
 
-/*
-    char line[50];
-    sprintf(line,"x.txt=\"%9.4f\"\xff\xff\xff",500.0*cos(t)+500.0);
-    display_PutString(line);
-    sprintf(line,"y.txt=\"%9.4f\"\xff\xff\xff",500.0*sin(t)+500.0);
-    display_PutString(line);
-    sprintf(line,"z.txt=\"%9.4f\"\xff\xff\xff",50.0*sin(t + 3.00)-50.0);
-    display_PutString(line);
-*/
+    LED_Write(!LED_Read());
 }    
  
 void lineProcess(char *line) {
@@ -71,7 +65,7 @@ void lineProcess(char *line) {
                 break;
             case 91:
                 display_PutString("distance.txt=\"G91\"\xff\xff\xff");
-                display_PutString("distancelabel.txt=\"incremental\"\xff\xff\xff");
+                display_PutString("distancelabel.txt=\"relative\"\xff\xff\xff");
                 break;
             case 54:
             case 55:
@@ -84,6 +78,50 @@ void lineProcess(char *line) {
                 break;
         }
     }               
+    if (c == 'M') {
+        switch (i) {
+            case 3:
+                display_PutString("spindle.txt=\"M3\"\xff\xff\xff");
+                display_PutString("spindlelabel.txt=\"spin on\"\xff\xff\xff");
+                break;
+            case 5:
+                display_PutString("spindle.txt=\"M5\"\xff\xff\xff");
+                display_PutString("spindlelabel.txt=\"spin off\"\xff\xff\xff");
+                break;
+            case 7:
+                display_PutString("coolant.txt=\"M7\"\xff\xff\xff");
+                display_PutString("coolantlabel.txt=\"cool on\"\xff\xff\xff");
+                break;
+            case 9:
+                display_PutString("coolant.txt=\"M9\"\xff\xff\xff");
+                display_PutString("coolantlabel.txt=\"cool off\"\xff\xff\xff");
+                break;
+        }
+    }
+    if (c == 'I') {
+        display_PutString("status.bco=2016\xff\xff\xff");
+        display_PutString("status.txt=\"idle\"\xff\xff\xff");
+    }
+    if (c == 'H') {
+        display_PutString("status.bco=65504\xff\xff\xff");
+        display_PutString("status.txt=\"hold\"\xff\xff\xff");
+    }
+    if (c == 'R') {
+        display_PutString("status.bco=32159\xff\xff\xff");
+        display_PutString("status.txt=\"running\"\xff\xff\xff");
+    }
+    if (c == 'A') {
+        display_PutString("status.bco=63488\xff\xff\xff");
+        display_PutString("status.txt=\"alarm\"\xff\xff\xff");
+    }
+    if (c == 'O') {
+        sprintf(line,"override.txt=\"%d%%\"\xff\xff\xff",i);
+        display_PutString(line);
+    }   
+    if (c == 'F') {
+        sprintf(line,"feed.txt=\"F%d\"\xff\xff\xff",i);
+        display_PutString(line);
+    }   
 }
 void displayProcessChar(uint8_t c) {
     if (c == '\n') {
@@ -125,19 +163,22 @@ int main(void)
 //   clockDisplay_Start();
     isrDisplay_StartEx(updateHandler);
     
-    console_PutString("nextion main\n");
+    console_PutString("\n\nnextion main\n");
     display_PutString("line.txt=\"nextion main\"\xff\xff\xff");
+    display_PutString("status.txt=\"running\"\xff\xff\xff");
 
     for(;;)
     {
         if ((c = display_GetChar()) != 0) { 
-            if ((c > 31 && c < 127) || c == '\r' || c == '\n') {        
+            if ((c > 31 && c < 127) || c == '\r' || c == '\n') {  
+                c = toupper(c);
                 displayProcessChar(c);
                 console_PutChar(c);
             }
         }
         if ((c = console_GetChar()) != 0) { 
             if ((c > 31 && c < 127) || c == '\r' || c == '\n') {        
+                c = toupper(c);
                 consoleProcessChar(c);
                 console_PutChar(c);
             }
@@ -146,5 +187,9 @@ int main(void)
         x = 500.0*cos(t)+500.0;
         y = 500.0*sin(t)+500.0;
         z = 50.0*sin(t+3.00)-50.0;
+        
+        while(SW_Read() == 0) {
+            LED_Write(1);
+        }
     }
 }
