ec89e8dc8d98228023fb6931f569c1640adc7655
Craig Hollabaugh <holla@gmail.com>
2018-09-16 23:29:58 -0600

    started adding keypad

--- src/controller/hyattController.cydsn/hyatt.c
+++ src/controller/hyattController.cydsn/hyatt.c
@@ -2,6 +2,18 @@
 
 #include "hyatt.h"
 
+CY_ISR(keyHandler) {
+    keyPending = 1;
+    Pin_IO_INT_ClearInterrupt();
+}
+
+
 void hyatt_init() {
     controllerConfig.axisSelected = X;
+}
+
+void hyatt_loop() {
+    if (keyPending) {
+
+    }
 }
--- src/controller/hyattController.cydsn/hyatt.h
+++ src/controller/hyattController.cydsn/hyatt.h
@@ -4,7 +4,36 @@
 #include <stdint.h>
     
 enum AXIS{X=1,Y,Z,A};
-    
+
+uint8_t keyPending;
+
+#define U3Addr      0x20
+#define U4Addr      0x21
+
+#define IOA_DIR     0x00
+#define IOA_POL     0x02
+#define IOA_GPINTEN 0x04
+#define IOA_DEFVAL  0x06
+#define IOA_INTCON  0x08
+#define IOA_CON     0x0A
+#define IOA_GPPU    0x0C
+#define IOA_INTF    0x0E
+#define IOA_INTCAP  0x10
+#define IOA_GPIOA   0x12
+#define IOA_OLAT    0x14
+
+#define IOB_DIR     0x01
+#define IOB_POL     0x03
+#define IOB_GPINTEN 0x05
+#define IOB_DEFVAL  0x07
+#define IOB_INTCON  0x09
+#define IOB_CON     0x0B
+#define IOB_GPPU    0x0D
+#define IOB_INTF    0x0F
+#define IOB_INTCAP  0x11
+#define IOB_GPIOB   0x13
+#define IOB_OLAT    0x15
+
 struct {
     uint8_t     axisSelected;
     uint8_t     feedOverride;
