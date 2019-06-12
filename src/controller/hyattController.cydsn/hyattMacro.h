1c507d0d5ad407c7faf7c243765b1fbddd61d1e5
Craig Hollabaugh <craig@hollabaugh.com>
2019-06-11 20:50:24 -0600

    added 2 macros

--- src/controller/hyattController.cydsn/hyattMacro.h
+++ src/controller/hyattController.cydsn/hyattMacro.h
@@ -8,16 +8,15 @@ struct macro {
 };    
     
 struct macro macros[CONTROLPANEL_SELECTIONCOUNTMAX] = {
-    // {"X0Y0Z1","X0Y0Z1G1F2500"},
     {"X0Y0Z1","G28G91Z0\nX0Y0G0G90\nZ1"},
     {"HOME","$H"},
     {"G28","G28G91Z0\nG28"},
     {"TCX0Y0","G10L20P1X0Y0"},
-    {"",""},
+    {"TCX0","G10L20P1X0"},
+    {"TCY0","G10L20P1Y0"},
     {"G28Set","G28.1"},
     {"G54","G54"},
     {"G55","G55"},
-    {"G56","G56"}
 };
     
 #endif
